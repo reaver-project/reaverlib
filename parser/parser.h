874b089304393fb7c8cbcfbd5aafcf95f95d5e19
Michał 'Griwes' Dominiak <griwes@griwes.info>
2013-04-08 23:38:20 +0200

    I wish C++ had `static if`.

--- parser/parser.h
+++ parser/parser.h
@@ -36,6 +36,51 @@ namespace reaver
 {
     namespace parser
     {
+        // TMP classes
+        template<typename T>
+        struct is_vector : public std::false_type
+        {
+        };
+
+        template<typename T, typename A>
+        struct is_vector<std::vector<T, A>> : public std::true_type
+        {
+        };
+
+        template<typename T>
+        struct is_tuple : public std::false_type
+        {
+        };
+
+        template<typename... Ts>
+        struct is_tuple<std::tuple<Ts...>> : public std::true_type
+        {
+        };
+
+        template<typename T, typename U>
+        struct make_tuple_type
+        {
+            using type = std::tuple<T, U>;
+        };
+
+        template<typename T, typename... Ts>
+        struct make_tuple_type<T, std::tuple<Ts...>>
+        {
+            using type = std::tuple<T, Ts...>;
+        };
+
+        template<typename... Ts, typename T>
+        struct make_tuple_type<std::tuple<Ts...>, T>
+        {
+            using type = std::tuple<Ts..., T>;
+        };
+
+        template<typename... T1s, typename... T2s>
+        struct make_tuple_type<std::tuple<T1s...>, std::tuple<T2s...>>
+        {
+            using type = std::tuple<T1s..., T2s...>;
+        };
+
         class parser
         {
         };
@@ -62,48 +107,77 @@ namespace reaver
             uint64_t _expected_type;
         };
 
-        // BIG TODO: template specializations so not and and parser are not handicapped
-        // i.e., lack of ParserType::value_type shouldn't stop it from being used in other parsers
-
         template<typename T>
         class not_parser : public parser
         {
+            using value_type = void;
         };
 
         template<typename T>
         class and_parser : public parser
         {
+            using value_type = void;
         };
 
         template<typename T>
         class optional_parser : public parser
         {
-            using value_type = boost::optional<typename T::value_type>;
+            using value_type = typename std::conditional<std::is_same<typename T::value_type, void>::value,
+                boost::optional<typename T::value_type>, void>::type;
         };
 
         template<typename T>
         class kleene_parser : public parser
         {
-            using value_type = std::vector<typename T::value_type>;
+            using value_type = typename std::conditional<std::is_same<typename T::value_type, void>::value,
+                std::vector<typename T::value_type>, void>::type;
         };
 
         template<typename T>
         class plus_parser : public parser
         {
-            using value_type = std::vector<typename T::value_type>;
+            using value_type = typename std::conditional<std::is_same<typename T::value_type, void>::value,
+                std::vector<typename T::value_type>, void>::type;
         };
 
         template<typename T, typename U>
         class variant_parser : public parser
         {
-            using value_type = boost::variant<typename T::value_type, typename U::value_type>;
+            using value_type = typename std::conditional<
+                std::is_same<typename T::value_type, void>::value,
+                typename std::conditional<
+                    std::is_same<typename U::value_type, void>::value,
+                    void,
+                    boost::optional<typename U::value_type>
+                >::type,
+                boost::variant<typename T::value_type, typename U::value_type>
+            >::type;
         };
 
         template<typename T, typename U>
         class sequence_parser : public parser
         {
-            using value_type = std::tuple<typename T::value_type, typename U::value_type>;
+            using value_type = typename std::conditional<
+                !std::is_same<typename T::value_type, void>::value
+                    && std::is_same<typename T::value_type, typename U::value_type>::value,
+                typename std::conditional<
+                    is_vector<typename T::value_type>::value,
+                    typename T::value_type,
+                    std::vector<typename T::value_type>
+                >::type,
+                typename std::conditional<
+                    std::is_same<typename T::value_type, void>::value,
+                    typename std::conditional<
+                        std::is_same<typename U::value_type, void>::value,
+                        void,
+                        std::vector<typename U::value_type>
+                    >::type,
+                    typename make_tuple_type<typename T::value_type, typename U::value_type>::type
+                >::type
+            >::type; // this is not even my final form! ...and probably some bugs on the way
         };
+
+        // TODO: finish and check this crazy template wankery
 
         template<typename T, typename U>
         class difference_parser : public parser
