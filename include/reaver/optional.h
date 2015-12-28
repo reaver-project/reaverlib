f15f63a0bb76cddff6cd9402eb756492c022e54a
Michał 'Griwes' Dominiak <griwes@griwes.info>
2015-12-28 23:27:47 +0100

    Introduce optional as a thin wrapper over variant.

---
+++ include/reaver/optional.h
@@ -0,0 +1,130 @@
+/**
+ * Reaver Library Licence
+ *
+ * Copyright © 2015 Michał "Griwes" Dominiak
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation is required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ *
+ **/
+
+#pragma once
+
+#include "variant.h"
+#include "invoke.h"
+
+namespace reaver { inline namespace _v1
+{
+    constexpr struct none_t {} none{};
+
+    template<typename T>
+    class optional : public variant<T, none_t>
+    {
+        using _base = variant<T, none_t>;
+    public:
+        optional() : _base{ none }
+        {
+        }
+
+        using _base::_base;
+        using _base::operator=;
+
+        operator bool() const
+        {
+            return _base::index() == 0;
+        }
+
+        T & operator*()
+        {
+            return get();
+        }
+
+        const T & operator*() const
+        {
+            return get();
+        }
+
+        T & get()
+        {
+            return reaver::get<0>(*this);
+        }
+
+        const T & get() const
+        {
+            return reaver::get<0>(*this);
+        }
+    };
+
+    template<typename T>
+    auto make_optional(T && t)
+    {
+        return optional<T>{ std::forward<T>(t) };
+    }
+
+    template<typename T, typename U>
+    bool operator==(const optional<T> & t, const U & u)
+    {
+        return t && *t == u;
+    }
+
+    template<typename T, typename U>
+    bool operator==(const T & t, const optional<U> & u)
+    {
+        return u && t == *u;
+    }
+
+    template<typename T, typename U>
+    bool operator==(const optional<T> & t, const optional<U> & u)
+    {
+        return (!t && !u) || (t && u && *t == *u);
+    }
+
+    template<typename T, typename U>
+    bool operator!=(const optional<T> & t, const U & u)
+    {
+        return !(t == u);
+    }
+
+    template<typename T, typename U>
+    bool operator!=(const T & t, const optional<U> & u)
+    {
+        return !(t == u);
+    }
+
+    template<typename T, typename U>
+    bool operator!=(const optional<T> & t, const optional<U> & u)
+    {
+        return !(t == u);
+    }
+
+    template<typename T, typename F>
+    decltype(auto) fmap(optional<T> & t, F && f)
+    {
+        return t ? make_optional(invoke(std::forward<F>(f), t.get())) : none;
+    }
+
+    template<typename T, typename F>
+    decltype(auto) fmap(const optional<T> & t, F && f)
+    {
+        return t ? make_optional(invoke(std::forward<F>(f), t.get())) : none;
+    }
+
+    template<typename T, typename F>
+    decltype(auto) fmap(optional<T> && t, F && f)
+    {
+        return t ? make_optional(invoke(std::forward<F>(f), std::move(t.get()))) : none;
+    }
+}}
+
---
+++ tests/optional.cpp
@@ -0,0 +1,69 @@
+/**
+ * Reaver Library Licence
+ *
+ * Copyright © 2015 Michał "Griwes" Dominiak
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation is required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ *
+ **/
+
+#include <cstddef>
+#include <type_traits>
+#include <memory>
+#include <functional>
+
+#include <reaver/exception.h>
+
+namespace test
+{
+#   include "optional.h"
+}
+
+#include <reaver/mayfly.h>
+
+MAYFLY_BEGIN_SUITE("optional");
+
+MAYFLY_ADD_TESTCASE("construction and comparisons", []()
+{
+    {
+        test::reaver::optional<int> o = test::reaver::none;
+        MAYFLY_CHECK(!o);
+    }
+
+    {
+        test::reaver::optional<int> o = 1;
+        MAYFLY_CHECK(o == 1);
+    }
+
+    {
+        test::reaver::optional<int> o = 1;
+        test::reaver::optional<int> p = 2;
+        test::reaver::optional<float> r = 1.f;
+
+        MAYFLY_CHECK(o != p);
+        MAYFLY_CHECK(o == r);
+        MAYFLY_CHECK(1.f == r);
+    }
+});
+
+MAYFLY_ADD_TESTCASE("fmap", []()
+{
+    MAYFLY_CHECK(test::reaver::fmap(test::reaver::make_optional(1), [](auto a){ return std::to_string(a); }) == "1");
+    MAYFLY_CHECK(!test::reaver::fmap(test::reaver::optional<int>{}, [](auto...){ return reaver::unit{}; }));
+});
+
+MAYFLY_END_SUITE;
+
