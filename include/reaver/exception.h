3bf799354e8b79a40ded1f13aa04b2a33bc0211a
Michał 'Griwes' Dominiak <griwes@griwes.info>
2016-03-18 21:06:25 +0100

    exception: added exception_list, in dire need of some tests.

--- include/reaver/exception.h
+++ include/reaver/exception.h
@@ -25,6 +25,7 @@
 #include <unordered_set>
 
 #include "logger.h"
+#include "prelude/functor.h"
 
 namespace reaver { inline namespace _v1
 {
@@ -112,6 +113,86 @@ namespace reaver { inline namespace _v1
 
         std::vector<logger::streamable> _streamables;
         mutable std::string _what;
+    };
+
+    class exception_list : public exception
+    {
+    public:
+        exception_list() : exception{ logger::always }
+        {
+        }
+
+        exception_list(const exception_list & other)
+        {
+            std::lock_guard<std::mutex> lock{ other._mutex };
+            _exceptions = other._exceptions;
+        }
+
+        exception_list(exception_list && other) : _exceptions{ std::move(other._exceptions) }
+        {
+        }
+
+        exception_list & operator=(const exception_list & other)
+        {
+            std::lock_guard<std::mutex> lock{ other._mutex };
+            std::lock_guard<std::mutex> self_lock{ _mutex };
+
+            _exceptions = other._exceptions;
+
+            return *this;
+        }
+
+        exception_list & operator=(exception_list && other)
+        {
+            std::lock_guard<std::mutex> lock{ _mutex };
+
+            _exceptions = std::move(other._exceptions);
+
+            return *this;
+        }
+
+        void push_back(std::exception_ptr ptr)
+        {
+            std::lock_guard<std::mutex> lock{ _mutex };
+            _exceptions.push_back(ptr);
+        }
+
+        std::size_t size() const
+        {
+            std::lock_guard<std::mutex> lock{ _mutex };
+            return _exceptions.size();
+        }
+
+        template<typename F>
+        auto map(F && f) const
+        {
+            std::lock_guard<std::mutex> lock{ _mutex };
+            return fmap(_exceptions, std::forward<F>(f));
+        }
+
+        virtual void print(reaver::logger::logger & l) const noexcept override
+        {
+            map([&](auto ptr) {
+                try
+                {
+                    std::rethrow_exception(ptr);
+                }
+                catch (reaver::exception & ex)
+                {
+                    ex.print(l);
+                }
+                catch (std::exception & ex)
+                {
+                    l(logger::fatal) << ex.what();
+                }
+
+                return unit{};
+            });
+        }
+
+    private:
+        mutable std::mutex _mutex;
+        std::vector<std::exception_ptr> _exceptions;
     };
 
     class file_is_directory : public exception
